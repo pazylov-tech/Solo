8292b06f77abb5a9ad75df81743afad6f1bf2346
pazylov-tech <pazylovtoichubek7@gmail.com>
2025-09-02 18:12:25 +0600

    Initial Commit

---
+++ Solo/SoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloApp.swift
+//  Solo
+//
+//  Created by Toichubek Pazylov on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
