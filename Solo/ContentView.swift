c035a3571de668e351667bedf1efbd945452b588
Toichubek Pazylov <pazylovtoichubek7@gmail.com>
2026-05-17 00:31:48 +0600

    first commit

---
+++ Solo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solo
+//
+//  Created by Pazylov Toichubek on 17/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, player. Ler's arise SOLO!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
