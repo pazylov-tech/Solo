f46596411bc1093cb5737ce8a5368ec69f71e1bd
Toichubek Pazylov <pazylovtoichubek7@gmail.com>
2026-05-19 18:16:23 +0600

    Keep building project folder structure

--- Solo/Views/Auth/AuthView.swift
+++ Solo/Views/Auth/AuthView.swift
@@ -2,14 +2,31 @@
 //  AuthView.swift
 //  Solo
 //
-//  Created by Pazylov Toichubek on 18/5/26.
-//
 
 import SwiftUI
 
 struct AuthView: View {
     var body: some View {
-        Text("AuthView is coming soon")
+        header
+    }
+}
+
+extension AuthView {
+    var header: some View {
+        VStack (spacing: 16) {
+            Image("solo-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 90, height: 90)
+                .shadow(color: .white.opacity(0.08), radius: 10, x: 0, y: 6)
+                .cornerRadius(15)
+            
+            Text("Auth Coming Soon")
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                .foregroundStyle(.black.opacity(0.9))
+        }
+        .padding(.horizontal, 24)
+        .padding(.vertical, 28)
     }
 }
 
