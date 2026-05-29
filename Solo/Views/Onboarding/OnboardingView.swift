9020ca8984b3b042a8482ce5c7171e224bff9217
Toichubek Pazylov <pazylovtoichubek7@gmail.com>
2026-05-29 20:53:03 +0600

    Started working on OnboardingView

--- Solo/Views/Onboarding/OnboardingView.swift
+++ Solo/Views/Onboarding/OnboardingView.swift
@@ -5,9 +5,54 @@
 
 import SwiftUI
 
+struct OnboardingPage {
+    let symbol: String
+    let symbolColor: Color
+    let bgColor: Color
+    let title: String
+    let subtitle: String
+}
+
+private let pages: [OnboardingPage] = [
+    OnboardingPage(
+        symbol: "cap",
+        symbolColor: .blue,
+        bgColor: Color.blue.opacity(0.05),
+        title: "Welcome to Solo Trip",
+        subtitle: "Build your habits, start simple, track your progress and reach momentum."
+    )
+]
+
+
 struct OnboardingView: View {
+    @State private var currentPage = 0
+    @State private var iconScale: CGFloat = 0.5
+    @State private var iconOpacity: Double = 0
+    @State private var isFloating = false
+    
     var body: some View {
-        Text("Onboarding View")
+        ZStack {
+            pages[currentPage].bgColor
+                .ignoresSafeArea()
+                .animation(.easeInOut(duration: 0.4), value: currentPage)
+            VStack {
+                
+                //Skip button
+                HStack {
+                    Spacer()
+                    Button("Skip") {
+                        //
+                    }
+                    .font(.subheadline).fontWeight(.medium)
+                    .foregroundColor(.gray)
+                    .padding(.top, 24)
+                    .padding(.horizontal, 24)
+                }
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
